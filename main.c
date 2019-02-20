8a7a910d5eb6ca815674e31c20f864ac7e446dd8
raultalm <raultalmacel@yahoo.com>
2019-02-20 17:32:36 +0100

    Add all original files

---
+++ main.c
@@ -0,0 +1,30 @@
+/**
+ * main.c
+ * 
+ * Ce fichier contient la fonction main() du programme de manipulation
+ * de fichiers pnm.
+ *
+ * @author: Nom Prenom Matricule
+ * @date: 
+ * @projet: INFO0030 Projet 1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <getopt.h>
+
+#include "pnm.h"
+
+
+int main(int argc, char *argv[]) {
+
+   char *optstring = "";
+
+   /* Insérez le code ici */
+
+   return 0;
+}
+
